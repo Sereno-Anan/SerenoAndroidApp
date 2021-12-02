53992798a499766a69e97d50ca7b3e2a1d3473a6
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-12-02 17:19:59 +0900

    add EmptyActivity

---
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/WeatherInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.sereno.serenoandroidapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sereno.serenoandroidapp.R
+
+class WeatherInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_info)
+    }
+}
