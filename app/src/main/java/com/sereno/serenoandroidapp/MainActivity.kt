c1a611a93801ce144ac3ed67a66290f17b7fad5b
kinoshita0923 <1196383@st.anan-nct.ac.jp>
2021-12-02 17:50:44 +0900

    画面遷移するボタンを作成

--- app/src/main/java/com/sereno/serenoandroidapp/MainActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sereno.serenoandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.sereno.serenoandroidapp.activities.PlayGifActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moveBtn = findViewById<Button>(R.id.moveBtn)
+
+        moveBtn.setOnClickListener {
+            val intent = Intent(this, PlayGifActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
