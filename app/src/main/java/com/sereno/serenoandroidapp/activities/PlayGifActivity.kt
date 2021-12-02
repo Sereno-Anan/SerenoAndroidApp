7a4ddf11f386a9c7942ae083acc165af8ebeaa45
kinoshita0923 <1196383@st.anan-nct.ac.jp>
2021-12-03 06:44:22 +0900

    PlayGifActivityにGifをループ再生

--- app/src/main/java/com/sereno/serenoandroidapp/activities/PlayGifActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/PlayGifActivity.kt
@@ -1,7 +1,9 @@
 package com.sereno.serenoandroidapp.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.sereno.serenoandroidapp.R
 
 class PlayGifActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class PlayGifActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_gif)
         setTitle(R.string.play_gif_name)
+
+        val playGifImage = findViewById<ImageView>(R.id.playGifImage)
+
+        Glide
+            .with(this)
+            .load(R.drawable.rain)
+            .into(playGifImage)
     }
 }
