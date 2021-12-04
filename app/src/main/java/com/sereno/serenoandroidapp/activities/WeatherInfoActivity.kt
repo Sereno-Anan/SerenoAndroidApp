bd47998dfae4fca6aada95ed9f380bad278a5e50
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-12-04 23:07:57 +0900

    Activityのコードの削除

--- app/src/main/java/com/sereno/serenoandroidapp/activities/WeatherInfoActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/WeatherInfoActivity.kt
@@ -1,7 +1,6 @@
 package com.sereno.serenoandroidapp.activities
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.sereno.serenoandroidapp.R
 
@@ -9,7 +8,6 @@ class WeatherInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_info)
-        val getWeatherButton = findViewById<Button>(R.id.getWeatherButton)
 
         setTitle(R.string.weather_info_name)
     }
