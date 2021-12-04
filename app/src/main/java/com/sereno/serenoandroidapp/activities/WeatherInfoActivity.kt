b16f35916d21cc7e7c32328411a5b194dbf28c97
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-12-04 23:04:52 +0900

    Activityのコードを削除

--- app/src/main/java/com/sereno/serenoandroidapp/activities/WeatherInfoActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/WeatherInfoActivity.kt
@@ -1,19 +1,9 @@
 package com.sereno.serenoandroidapp.activities
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.sereno.serenoandroidapp.BuildConfig
 import com.sereno.serenoandroidapp.R
-import com.sereno.serenoandroidapp.services.OpenWeatherMapService
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import kotlin.concurrent.thread
 
 class WeatherInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,36 +12,5 @@ class WeatherInfoActivity : AppCompatActivity() {
         val getWeatherButton = findViewById<Button>(R.id.getWeatherButton)
 
         setTitle(R.string.weather_info_name)
-        getWeatherButton.setOnClickListener {
-            getCurrentWeather()
-        }
-    }
-
-    fun getCurrentWeather() {
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        thread {
-            try {
-                val service: OpenWeatherMapService =
-                    retrofit.create(OpenWeatherMapService::class.java)
-                val WeatherApiResponse = service.fetchWeather(
-                    "anan", BuildConfig.OWM_API_KEY, "metric", "ja"
-                ).execute().body()
-                    ?: throw IllegalStateException("bodyがnull")
-
-                Handler(Looper.getMainLooper()).post {
-                    Log.d("response-weather", WeatherApiResponse.weather.toString())
-                }
-            } catch (e: Exception) {
-                Log.d("response-weather", "debug $e")
-            }
-        }
     }
 }
