fcb3ad6b7c8a5ddbd6838e9b0ec1356017e26dd9
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-12-07 19:44:46 +0900

    Firebase RTDBからGETする処理を追加

--- app/src/main/java/com/sereno/serenoandroidapp/activities/FirebaseTestActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/FirebaseTestActivity.kt
@@ -1,17 +1,24 @@
 package com.sereno.serenoandroidapp.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.sereno.serenoandroidapp.R
 import com.sereno.serenoandroidapp.models.db.firestore.RainDrops
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FirebaseTestActivity : AppCompatActivity() {
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_test)
@@ -19,11 +26,16 @@ class FirebaseTestActivity : AppCompatActivity() {
 
         val rootLayout: View = findViewById(android.R.id.content)
 
-        val db = FirebaseFirestore.getInstance()
+        val firestore = FirebaseFirestore.getInstance()
+        val rtdb = Firebase.database.reference
+
         val addFirebaseDataButton = findViewById<Button>(R.id.addFirebaseDataButton)
+        val getFirebaseDataButton = findViewById<Button>(R.id.getFirebaseDataButton)
+        val raindropsDateText = findViewById<TextView>(R.id.raindropsDateText)
+        val raindropsStatusText = findViewById<TextView>(R.id.raindropsStatusText)
 
         addFirebaseDataButton.setOnClickListener {
-            db.collection("raindrops")
+            firestore.collection("raindrops")
                 .document()
                 .set(RainDrops(true, FieldValue.serverTimestamp()))
                 .addOnSuccessListener {
@@ -34,6 +46,18 @@ class FirebaseTestActivity : AppCompatActivity() {
                     Log.w("Firebase Log", "Error sending documents.", it)
                     Snackbar.make(rootLayout, "Failure", Snackbar.LENGTH_LONG).show()
                 }
+        }
+
+        getFirebaseDataButton.setOnClickListener {
+            rtdb.child("raindrops/").get().addOnSuccessListener {
+                val date = Date(it.child("timestamp").value as Long)
+                val formattedDate = SimpleDateFormat("yyyy/MM/dd hh:mm")
+                raindropsDateText.text = formattedDate.format(date)
+                raindropsStatusText.text = it.child("status").value.toString()
+            }.addOnFailureListener {
+                Log.e("firebase", "Error getting data", it)
+                Snackbar.make(rootLayout, "Failure getting data", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 }
