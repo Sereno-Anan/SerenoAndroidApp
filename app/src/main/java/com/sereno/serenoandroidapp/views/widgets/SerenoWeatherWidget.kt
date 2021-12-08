8d5a3f5b8c0d419f9cbfde0ef08ff39ef25a7d57
kinoshita0923 <sota0923ks15@gmail.com>
2021-12-08 23:55:40 +0900

    背景の切り替える処理を作成

--- app/src/main/java/com/sereno/serenoandroidapp/views/widgets/SerenoWeatherWidget.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/views/widgets/SerenoWeatherWidget.kt
@@ -3,8 +3,15 @@ package com.sereno.serenoandroidapp.views.widgets
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.util.Log
 import android.widget.RemoteViews
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.sereno.serenoandroidapp.R
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
 
 /**
  * Implementation of App Widget functionality.
@@ -38,6 +45,21 @@ internal fun updateAppWidget(
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget_sereno_weather)
 //    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    val rtdb = Firebase.database.reference
+
+    rtdb.child("raindrops/").get().addOnSuccessListener {
+        val weatherStatus = it.child("status").value
+        if (weatherStatus == true) {
+            views.setInt(R.id.widgetBackground, "setBackgroundResource",R.drawable.image_rain)
+        } else {
+            views.setInt(R.id.widgetBackground, "setBackgroundResource",R.drawable.image_sunny)
+        }
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+        Log.d("response-status", it.child("status").value.toString())
+    }.addOnFailureListener {
+        Log.e("firebase", "Error getting data", it)
+    }
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
