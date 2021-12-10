b3b370399869b3da91731166799b0fb57a6b832a
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-12-10 21:20:12 +0900

    天気の場所を表示するように変更

--- app/src/main/java/com/sereno/serenoandroidapp/views/widgets/SerenoWeatherWidget.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/views/widgets/SerenoWeatherWidget.kt
@@ -3,8 +3,6 @@ package com.sereno.serenoandroidapp.views.widgets
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
 import android.util.Log
 import android.widget.RemoteViews
 import com.google.firebase.database.ktx.database
@@ -12,6 +10,7 @@ import com.google.firebase.ktx.Firebase
 import com.sereno.serenoandroidapp.R
 import java.util.*
 import kotlin.concurrent.schedule
+
 
 /**
  * Implementation of App Widget functionality.
@@ -22,6 +21,7 @@ class SerenoWeatherWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -44,19 +44,28 @@ internal fun updateAppWidget(
 ) {
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget_sereno_weather)
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
-
+    val pref = context.getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
     val rtdb = Firebase.database.reference
 
     // 3分ごとに取得
     Timer().schedule(1000, 180000) {
+        if (pref.getString("cityName", "")!!.isNotEmpty()) {
+            views.setTextViewText(R.id.widgetCurrentCityName, pref.getString("cityName", ""))
+        }
         rtdb.child("raindrops/").get().addOnSuccessListener {
             val weatherStatus = it.child("status").value
-
             if (weatherStatus == true) {
-                views.setInt(R.id.widgetBackground, "setBackgroundResource", R.drawable.background_style_rain)
+                views.setInt(
+                    R.id.widgetBackground,
+                    "setBackgroundResource",
+                    R.drawable.background_style_rain
+                )
             } else {
-                views.setInt(R.id.widgetBackground, "setBackgroundResource", R.drawable.background_style_sunny)
+                views.setInt(
+                    R.id.widgetBackground,
+                    "setBackgroundResource",
+                    R.drawable.background_style_sunny
+                )
             }
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
