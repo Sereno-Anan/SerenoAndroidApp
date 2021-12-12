9df4a1cc6cc9ae50acc0cf38adbcd6591cbebb5c
kinoshita0923 <sota0923ks15@gmail.com>
2021-12-12 14:53:54 +0900

    ToastからSnackbarに変更

--- app/src/main/java/com/sereno/serenoandroidapp/activities/ConfigureActivity.kt
+++ app/src/main/java/com/sereno/serenoandroidapp/activities/ConfigureActivity.kt
@@ -3,12 +3,11 @@ package com.sereno.serenoandroidapp.activities
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.sereno.serenoandroidapp.BuildConfig
 import com.sereno.serenoandroidapp.R
 import com.sereno.serenoandroidapp.services.OpenWeatherMapService
@@ -42,6 +41,8 @@ class ConfigureActivity : AppCompatActivity() {
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
 
+        val rootLayout: View = findViewById(android.R.id.content)
+
         thread {
             try {
                 val service: OpenWeatherMapService =
@@ -56,11 +57,8 @@ class ConfigureActivity : AppCompatActivity() {
                     apply()
                 }
             } catch (e: Exception) {
-                Handler(Looper.getMainLooper()).post {
-                    val toast =
-                        Toast.makeText(this, "This city name is not found", Toast.LENGTH_SHORT)
-                    toast.show()
-                }
+                Snackbar.make(rootLayout, "This city name is not found", Snackbar.LENGTH_LONG).show()
+
                 Log.d("response-error", "debug $e")
             }
         }
